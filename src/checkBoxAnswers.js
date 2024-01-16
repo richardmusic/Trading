91729ae73082db9b52e4804674b8d64d97787385
richardmusic <146582773+richardmusic@users.noreply.github.com>
2024-01-16 02:31:06 -0700

    Update checkBoxAnswers.js

--- src/checkBoxAnswers.js
+++ src/checkBoxAnswers.js
@@ -1,16 +1,46 @@
 export const checkBoxAnswers = [
-    ['Y', 'N', 'Other'], /*  Line 1 */  ['Y', 'N', 'Other'], /*  Line 2 */  ['Y', 'N', 'Other'], /*  Line 3 */
-    ['Y', 'N', 'Other'], /*  Line 4 */  ['Consistent Direction/Clear Trending', 'Mixed', 'Other'], /*  Line 5 */  ['Above', 'Below', 'Around'], /*  Line 6 */
-    ['Y', 'N', 'Other'], /*  Line 7 */  ['Y', 'N', 'Other'], /*  Line 8 */  ['Inside', 'Outside', 'Other'], /*  Line 9 */
-    ['Y', 'N', 'Other'], /*  Line 10 */  ['1', '2', '3'], /*  Line 11 */  ['1', '2', '3'], /*  Line 12 */
-    ['Y', 'N', 'Other'], /*  Line 13 */  ['Bullish', 'Bearish', 'Cant tell/Ranging'], /*  Line 14 */  ['Bullish', 'Bearish', 'Cant tell/Ranging'], /*  Line 15 */
-    ['Bullish', 'Bearish', 'Cant tell/Ranging'], /*  Line 16 */  ['Bullish', 'Bearish', 'Other'], /*  Line 17 */  ['Y', 'N', 'Other'], /*  Line 18 */
-    ['Y', 'N', 'Other'], /*  Line 19 */  ['Bullish', 'Bearish', 'Other'], /*  Line 20 */  ['Bullish', 'Bearish', 'Other'], /*  Line 21 */
-    ['Bullish', 'Bearish', 'Other'], /*  Line 22 */  ['Y', 'N', 'Other'], /*  Line 23 */  ['Y', 'N', 'Other'], /*  Line 24 */
-    ['Y', 'N', 'Other'], /*  Line 25 */  ['Y', 'N', 'Other'], /*  Line 26 */  ['Y', 'N', 'Other'], /*  Line 27 */
-    ['Bullish Trend', 'Bullish Reversal', 'Consolidation'], /*  Line 28 */  ['Tues H/L of week', 'Wed H/L of Week', 'Consolidation first half'], /*  Line 29 */  ['Bullish', 'Bearish', 'Other'], /*  Line 30 */
-    ['Bullish', 'Bearish', 'Other'], /*  Line 31 */  ['Bullish', 'Bearish', 'Other'], /*  Line 32 */  ['Bullish', 'Bearish', 'Other'], /*  Line 33 */
-    ['Bullish', 'Bearish', 'Other'], /*  Line 34 */  ['Y', 'N', 'Other'], /*  Line 35 */  ['1', '2', '3'], /*  Line 36 */
-    ['1', '2', '3'], /*  Line 37 */  ['Manipulating', 'Accumulating', 'Trending'], /*  Line 38 */  ['Y', 'N', 'Other'], /*  Line 39 */
-    ['Y', 'N', 'Other'], /*  Line 40 */  ['Y', 'N', 'Other'], /*  Line 41 */  ['.30% 50pt', '.50% 75pt', '1.00% 100pt'], /*  Line 42 */
-    ['.30% 50pt', '.50% 75pt', '1.00% 100pt'], /*  Line 43 */  ['.75% 100pt', '1.50% 150pt', '2.00% 200pt'], /*  Line 44 */ ['Y', 'N', 'Other'], /*  Line 45 */];
+  ['Y', 'N', 'Other'],                             /*  Line 1  */
+  ['Y', 'N', 'Other'],                             /*  Line 2  */
+  ['Y', 'N', 'Other'],                             /*  Line 3  */
+  ['Y', 'N', 'Other'],                             /*  Line 4  */
+  ['Consistent Direction/Clear Trending', 'Mixed', 'Other'],  /*  Line 5  */
+  ['Above', 'Below', 'Around'],                    /*  Line 6  */
+  ['Y', 'N', 'Other'],                             /*  Line 7  */
+  ['Y', 'N', 'Other'],                             /*  Line 8  */
+  ['Inside', 'Outside', 'Other'],                  /*  Line 9  */
+  ['Y', 'N', 'Other'],                             /*  Line 10 */
+  ['1', '2', '3'],                                 /*  Line 11 */
+  ['1', '2', '3'],                                 /*  Line 12 */
+  ['Y', 'N', 'Other'],                             /*  Line 13 */
+  ['Bullish', 'Bearish', 'Cant tell/Ranging'],    /*  Line 14 */
+  ['Bullish', 'Bearish', 'Cant tell/Ranging'],    /*  Line 15 */
+  ['Bullish', 'Bearish', 'Cant tell/Ranging'],    /*  Line 16 */
+  ['Bullish', 'Bearish', 'Other'],                /*  Line 17 */
+  ['Y', 'N', 'Other'],                             /*  Line 18 */
+  ['Y', 'N', 'Other'],                             /*  Line 19 */
+  ['Bullish', 'Bearish', 'Other'],                /*  Line 20 */
+  ['Bullish', 'Bearish', 'Other'],                /*  Line 21 */
+  ['Bullish', 'Bearish', 'Other'],                /*  Line 22 */
+  ['Y', 'N', 'Other'],                             /*  Line 23 */
+  ['Y', 'N', 'Other'],                             /*  Line 24 */
+  ['Y', 'N', 'Other'],                             /*  Line 25 */
+  ['Y', 'N', 'Other'],                             /*  Line 26 */
+  ['Discount', 'Premium', 'Other'],                             /*  Line 27 */
+  ['Bullish Trend', 'Bullish Reversal', 'Consolidation'], /*  Line 28 */
+  ['Tues H/L week', 'Wed H/L week', 'Consolidation 1st half'], /*  Line 29 */
+  ['Bullish', 'Bearish', 'Other'],                /*  Line 30 */
+  ['Bullish', 'Bearish', 'Other'],                /*  Line 31 */
+  ['Bullish', 'Bearish', 'Other'],                /*  Line 32 */
+  ['Bullish', 'Bearish', 'Other'],                /*  Line 33 */
+  ['Bullish', 'Bearish', 'Other'],                /*  Line 34 */
+  ['Y', 'N', 'Other'],                             /*  Line 35 */
+  ['1', '2', '3'],                                 /*  Line 36 */
+  ['1', '2', '3'],                                 /*  Line 37 */
+  ['Manipulating', 'Accumulating', 'Trending'],    /*  Line 38 */
+  ['Y', 'N', 'Other'],                             /*  Line 39 */
+  ['Y', 'N', 'Other'],                             /*  Line 40 */
+  ['Y', 'N', 'Other'],                             /*  Line 41 */
+  ['.30% 50pt', '.50% 75pt', '1.00% 100pt'],      /*  Line 42 */
+  ['.30% 50pt', '.50% 75pt', '1.00% 100pt'],      /*  Line 43 */
+  ['.75% 100pt', '1.50% 150pt', '2.00% 200pt'],   /*  Line 44 */
+  ['Y', 'N', 'Other'],                             /*  Line 45 */];
