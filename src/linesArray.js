22dfb26cc19d9c44a153e6cfec2923aa2f49dfe4
richardmusic <146582773+richardmusic@users.noreply.github.com>
2024-01-16 15:45:51 -0700

    Add files via upload

---
+++ src/linesArray.js
@@ -0,0 +1,47 @@
+export const lines = [        
+  'Flat Highs/Lows, after a Stop run (15m)......',                           /*  Line 1  */ 
+  'Judas Swing Happened Liquidity Swept',                              /*  Line 2  */ 
+  'Now Power Hour',                                                    /*  Line 3  */ 
+  'MTW thurs necessary......',                                       /*  Line 4  */ 
+  'ES/NQ Moving Together',                                          /*  Line 5  */ 
+  'VWAP',                                                              /*  Line 6  */ 
+  'Buy/Sellside objectives taken before NY open......',                        /*  Line 7  */ 
+  'London swept Asia high/low......',                                         /*  Line 8  */ 
+  'NY Opened within yesterday Day Candle......',                              /*  Line 9  */ 
+  'Yesterday a Large Range Day......',                                        /*  Line 10  */ 
+  'Consecutive Days Trending - over 1% SP500',                          /*  Line 11 */ 
+  'Consecutive Days Ranging - >.50%',                                  /*  Line 12 */ 
+  'Medium/Higher Impact Economic Calendar Event',                        /*  Line 13 */ 
+  'Seasonal Tendency......',                                                  /*  Line 14 */ 
+  'ES Looking More......',                                                /*  Line 15 */ 
+  'NQ Looking More......',                                                /*  Line 16 */ 
+  'Have a Daily Bias',                                                 /*  Line 17 */ 
+  'Weekly Candle Bodies small, compressed......',                     /*  Line 18 */ 
+  'Did Accumulation / Ranging Happen',                                  /*  Line 19 */ 
+  'Discords Opinion',                                                  /*  Line 20 */ 
+  'Topstep People Opinion',                                            /*  Line 21 */ 
+  'TopTech Direction',                                                 /*  Line 22 */ 
+  'Think London is the Low/High of the Day',                            /*  Line 23 */ 
+  'Think London was a Reversal',                                       /*  Line 24 */ 
+  'Think NY is a continuation',                                        /*  Line 25 */ 
+  'Think NY is a Reversal',                                            /*  Line 26 */ 
+  'Weekly Prof 🔻 ......',                                            /*  Line 27 */ 
+  'Weekly Prof ✅ .......',                                             /*  Line 28 */ 
+  'Potential Daily Patterns......',                                      /*  Line 29 */ 
+  'Monthly Bias/Direction/Targets',                                     /*  Line 30 */ 
+  'Weekly Bias/Direction/Targets',                                      /*  Line 31 */ 
+  'Daily Bias/Direction/Targets',                                       /*  Line 32 */ 
+  '4H Bias/Direction/Targets',                                          /*  Line 33 */ 
+  'SMT Divergence Intraday/Daily',                                      /*  Line 34 */ 
+  'Xray Levels- IFVG/Break Blocks/Order Blocks etc',                    /*  Line 35 */ 
+  'Consecutive Weeks Trending',                                         /*  Line 36 */ 
+  'Consecutive Weekly Wicks',                                           /*  Line 37 */ 
+  'Manipulating Accumulating Trending',                              /*  Line 38 */ 
+  'Multiple NWOGs crowding same area',                                   /*  Line 39 */ 
+  'Large range overnight-avoid AM, only PM......',                             /*  Line 40 */ 
+  'Bank Holiday/the Day after/Half Day',                                /*  Line 41 */ 
+  'How High is ES target from bottom',                                  /*  Line 42 */ 
+  'How High is ES target from VWAP',                                    /*  Line 43 */ 
+  'How High % to total Daily target',                                   /*  Line 44 */  
+  'First/Last few days of month',                                       /*  Line 45 */  
+];
---
+++ src/toolTips.js
@@ -0,0 +1,65 @@
+import image1 from './jpgs/image1.png'; import image2 from './jpgs/image2.png'; import image3 from './jpgs/image3.png'
+import image4 from './jpgs/image4.png'; import image5 from './jpgs/image5.png'; import image6 from './jpgs/image6.png'
+import image9 from './jpgs/Jan 16-20 March ES.png'; import image10 from './jpgs/Jan 16-20 Overall ES.png'; import image7 from './jpgs/Jan 16-20 March NQ.png';
+import image8 from './jpgs/Jan 16-20 Overall NQ.png'; import image11 from './jpgs/sixMonth All ES.png'; import image12 from './jpgs/sixMonth All NQ.png';
+import image13 from './jpgs/Year SP500 All.png'; import image14 from './jpgs/Year NQ All.png';
+import weekly1 from './jpgs/1Bullish - Tues Low of Week.png';
+import weekly2 from './jpgs/2Consolidate Midweek rally - End Bull.png';
+import weekly3 from './jpgs/3Bullish - Wed Low of Week.png';
+import weekly4 from './jpgs/4Wed Weekly Reversal - End Bull.png';
+import weekly5 from './jpgs/5Consolidate Thurs Reversal - End Bull.png';
+import weekly6 from './jpgs/6Seek Destroy - Ends Bullish.png';
+import weekly7 from './jpgs/7Bearish - Tues High of Week.png';
+import weekly8 from './jpgs/8Consolidate Midweek Decline - End Bear.png';
+import weekly9 from './jpgs/9Bearish - Wed High of Week.png';
+import weekly10 from './jpgs/10Wed Weekly Reversal - End Bear.png';
+import weekly11 from './jpgs/11Consolidate Thurs Reversal - End Bear.png';
+import weekly12 from './jpgs/12Seek Destroy - Ends Bearish.png';
+
+export const toolTips = [
+    "First, a Stop Run",            /*Line 1*/
+    "",    /*Line 2*/
+    "",  /*Line 3*/
+    "Tues usually High/Low of week",    /*Line 4*/
+    "",      /*Line 5*/
+    "",       /*Line 6*/
+    "Seek and Destroy",   /*Line 7*/
+    "Seek and Destroy",   /*Line 8*/
+    "Possibly Seek and Destroy, wait for PDH/PDL to be tagged before trading",   /*Line 9*/
+    "Possibly Seek and Destroy, Avoid AM, trade PM",     /*Line 10*/
+    "",      /*Line 11*/
+    "",   /*Line 12*/
+    "",  /*Line 13*/
+    [image11, image12, image13, image14],   /*Line 14*/
+    [image7, image8],     /*Line 15*/
+    [image9, image10],    /*Line 16*/
+    "",    /*Line 17*/
+    "Ignore wicks, Big days come after small bodies",               /*Line 18*/
+    "",     /*Line 19*/
+    "",  /*Line 20*/
+    "",    /*Line 21*/
+    "",     /*Line 22*/
+    "", /*Line 23*/
+    "",     /*Line 24*/
+    "",   /*Line 25*/
+    "",    /*Line 26*/
+    [weekly7, weekly8, weekly9, weekly10, weekly11, weekly12],      /*Line 27*/
+    [weekly1, weekly2, weekly3, weekly4, weekly5, weekly6],          /*Line 28*/
+    [image1, image2, image3, image4, image5, image6],                /*Line 29*/
+    "",    /*Line 30*/
+    "",      /*Line 31*/
+    "",    /*Line 32*/
+    "", /*Line 33*/
+    "",    /*Line 34*/
+    "",    /*Line 35*/
+    "",  /*Line 36*/
+    "", /*Line 37*/
+    "",    /*Line 38*/
+    "",    /*Line 39*/
+    "Possibly Seek and Destroy",                                    /*Line 40*/
+    "",    /*Line 41*/   
+    "",    /*Line 42*/
+    "",     /*Line 43*/
+    "",    /*Line 44*/
+    ""        /*Line 45*/
+];
