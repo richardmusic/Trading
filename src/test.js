b2d1d35a48844a5098f5f639813bb27b04cdd423
richardmusic <146582773+richardmusic@users.noreply.github.com>
2024-01-20 14:54:09 -0700

    Add files via upload

---
+++ src/test.js
@@ -0,0 +1,116 @@
+
+    
+    
+  return (<>
+      <div className="radio-group"><p>Judas Swing Happened? Liquidity Swept?</p><div>
+        <input type="radio" name="judas" value="yes" id="yes"  />
+            <label htmlFor="yes">Yes</label></div><div>
+        <input type="radio" name="judas" value="no" id="no" />
+            <label htmlFor="no">No</label></div></div>
+
+    <div className="radio-group"><p>Judas Swing Happened? Liquidity Swept?</p><div>
+        <input type="radio" name="judas" value="yes" id="yes"  />
+            <label htmlFor="yes">Yes</label></div><div>
+        <input type="radio" name="judas" value="no" id="no" />
+            <label htmlFor="no">No</label></div></div>
+    <div className="radio-group"><p>Now Power Hour?</p><div>
+        <input type="radio" name="power" value="yes" id="yes" />
+            <label htmlFor="yes">Yes</label></div><div>
+        <input type="radio" name="power" value="no" id="no" />
+            <label htmlFor="no">No</label></div></div>
+    <div className="radio-group"><p>Mon, Tues, Wed? Thurs if nothing happened? - Tues usually high/low of week</p><div>
+        <input type="radio" name="power" value="yes" id="yes" />
+            <label htmlFor="yes">Yes</label></div><div>
+        <input type="radio" name="power" value="no" id="no" />
+            <label htmlFor="no">No</label></div></div>
+    <div className="radio-group"><p>Mixed Day?</p><div>
+        <input type="radio" name="power" value="yes" id="yes" />
+            <label htmlFor="yes">Mixed</label></div><div>
+        <input type="radio" name="power" value="no" id="no" />
+            <label htmlFor="no">Consistent Direction/Clear Trending</label></div></div>
+    <div className="radio-group"><p>VWAP</p><div>
+        <input type="radio" name="vwap" value="above" id="above" />
+            <label htmlFor="above">Above</label></div><div>
+        <input type="radio" name="vwap" value="below" id="below" />
+            <label htmlFor="below">Below</label></div><div>
+        <input type="radio" name="vwap" value="around" id="around" />
+            <label htmlFor="around">Around</label></div></div>
+    <div className="radio-group"><p>Buy/Sellside objectives taken before NY open?</p><div>
+        <input type="radio" name="power" value="yes" id="yes" />
+            <label htmlFor="yes">Yes</label></div><div>
+        <input type="radio" name="power" value="no" id="no" />
+            <label htmlFor="no">No</label></div></div>
+    <div className="radio-group"><p>London swept Asia high/low?</p><div>
+        <input type="radio" name="power" value="yes" id="yes" />
+            <label htmlFor="yes">Yes</label></div><div>
+        <input type="radio" name="power" value="no" id="no" />
+            <label htmlFor="no">No</label></div></div>
+    <div className="radio-group"><p>NY Opened within yesterday Day Candle?</p><div>
+        <input type="radio" name="power" value="yes" id="yes" />
+            <label htmlFor="yes">Within</label></div><div>
+        <input type="radio" name="power" value="no" id="no" />
+            <label htmlFor="no">Outside</label></div></div>
+    <div className="radio-group"><p>Yesterday a Large Range Day?</p><div>
+        <input type="radio" name="power" value="yes" id="yes" />
+            <label htmlFor="yes">Yes</label></div><div>
+        <input type="radio" name="power" value="no" id="no" />
+            <label htmlFor="no">No</label></div></div>
+    <div className="radio-group"><p>Consecutive Days Trending - over 1% SP500</p><div>
+        <input type="radio" name="power" value="option1" id="option1" />
+            <label htmlFor="option1">1</label></div><div>
+        <input type="radio" name="power" value="option2" id="option2" />
+        <label htmlFor="option2">2</label></div><div>
+        <input type="radio" name="power" value="option3" id="option3" />
+        <label htmlFor="option3">3</label></div><div>
+        <input type="radio" name="power" value="option4" id="option4" />
+        <label htmlFor="option4">4</label></div><div>
+        <input type="radio" name="power" value="option5" id="option5" />
+        <label htmlFor="option5">5</label></div></div>
+    <div className="radio-group"><p>Consecutive Days Ranging - >.50%</p><div>
+        <input type="radio" name="power" value="button1" id="button1" />
+        <label htmlFor="button1">1</label></div><div>
+        <input type="radio" name="power" value="button2" id="button2" />
+        <label htmlFor="button2">2</label></div><div>
+        <input type="radio" name="power" value="button3" id="button3" />
+        <label htmlFor="button3">3</label></div><div>
+        <input type="radio" name="power" value="button4" id="button4" />
+        <label htmlFor="button4">4</label></div><div>
+        <input type="radio" name="power" value="button5" id="button5" />
+        <label htmlFor="button5">5</label></div></div>
+    <div className="radio-group"><p>Medium/Higher Impact Economic Calendar Event?</p><div>
+        <input type="radio" name="power" value="yes" id="yes" />
+            <label htmlFor="yes">Yes</label></div><div>
+        <input type="radio" name="power" value="no" id="no" />
+            <label htmlFor="no">No</label></div></div>    
+    <div className="radio-group"><p>Seasonal Tendency</p><div>
+    <input type="radio" name="vwap" value="above" id="above" />
+            <label htmlFor="above">Bullish</label></div><div>
+        <input type="radio" name="vwap" value="below" id="below" />
+            <label htmlFor="below">Bearish</label></div><div>
+        <input type="radio" name="vwap" value="around" id="around" />
+            <label htmlFor="around">Don't Know/Ranging</label></div></div>   
+    <div className="radio-group"><p>ES Looking More...</p><div>
+    <input type="radio" name="vwap" value="above" id="above" />
+            <label htmlFor="above">Bullish</label></div><div>
+        <input type="radio" name="vwap" value="below" id="below" />
+            <label htmlFor="below">Bearish</label></div><div>
+        <input type="radio" name="vwap" value="around" id="around" />
+            <label htmlFor="around">Can't tell/Ranging</label></div></div>   
+    <div className="radio-group"><p>NQ Looking More...</p><div>
+    <input type="radio" name="vwap" value="above" id="above" />
+            <label htmlFor="above">Bullish</label></div><div>
+        <input type="radio" name="vwap" value="below" id="below" />
+            <label htmlFor="below">Bearish</label></div><div>
+        <input type="radio" name="vwap" value="around" id="around" />
+            <label htmlFor="around">Can't tell/Ranging</label></div></div>   
+    <div className="radio-group"><p>Have a Daily Bias?</p><div>
+        <input type="radio" name="vwap" value="above" id="above" />
+            <label htmlFor="above">Bullish</label></div><div>
+        <input type="radio" name="vwap" value="below" id="below" />
+            <label htmlFor="below">Bearish</label></div><div>
+        <input type="radio" name="vwap" value="around" id="around" />
+            <label htmlFor="around">Don't Know/Ranging</label></div></div>
+    </>);}
+
+
+
