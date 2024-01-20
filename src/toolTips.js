a20b147d6106e048e074d47bca919047fa0a72db
richardmusic <146582773+richardmusic@users.noreply.github.com>
2024-01-20 09:18:19 -0700

    Update toolTips.js

--- src/toolTips.js
+++ src/toolTips.js
@@ -1,8 +1,13 @@
 import image1 from './jpgs/image1.png'; import image2 from './jpgs/image2.png'; import image3 from './jpgs/image3.png'
 import image4 from './jpgs/image4.png'; import image5 from './jpgs/image5.png'; import image6 from './jpgs/image6.png'
-import image9 from './jpgs/Jan 16-20 March ES.png'; import image10 from './jpgs/Jan 16-20 Overall ES.png'; import image7 from './jpgs/Jan 16-20 March NQ.png';
-import image8 from './jpgs/Jan 16-20 Overall NQ.png'; import image11 from './jpgs/sixMonth All ES.png'; import image12 from './jpgs/sixMonth All NQ.png';
-import image13 from './jpgs/Year SP500 All.png'; import image14 from './jpgs/Year NQ All.png';
+import image7 from './jpgs/real/Jan 16-20 March NQ.png';
+import image8 from './jpgs/real/Jan 16-20 Overall NQ.png'; 
+import image9 from './jpgs/real/Jan 16-20 March ES.png'; 
+import image10 from './jpgs/real/Jan 16-20 Overall ES.png'; 
+import image11 from './jpgs/real/sixMonth All ES.png'; 
+import image12 from './jpgs/real/sixMonth All NQ.png';
+import image13 from './jpgs/real/Year SP500 All.png'; 
+import image14 from './jpgs/real/Year NQ All.png';
 import weekly1 from './jpgs/1Bullish - Tues Low of Week.png';
 import weekly2 from './jpgs/2Consolidate Midweek rally - End Bull.png';
 import weekly3 from './jpgs/3Bullish - Wed Low of Week.png';
@@ -15,7 +20,11 @@ import weekly9 from './jpgs/9Bearish - Wed High of Week.png';
 import weekly10 from './jpgs/10Wed Weekly Reversal - End Bear.png';
 import weekly11 from './jpgs/11Consolidate Thurs Reversal - End Bear.png';
 import weekly12 from './jpgs/12Seek Destroy - Ends Bearish.png';
-
+import avgDaily from './jpgs/AVG Daily.png';
+import avgWeekly from './jpgs/AVG Weekly.png';
+import fake14 from './jpgs/Fake14.png';
+import fake15 from './jpgs/Fake15.png';
+import fake16 from './jpgs/Fake16.png';
 export const toolTips = [
     "First, a Stop Run",            /*Line 1*/
     "",    /*Line 2*/
@@ -30,9 +39,9 @@ export const toolTips = [
     "",      /*Line 11*/
     "",   /*Line 12*/
     "",  /*Line 13*/
-    [image11, image12, image13, image14],   /*Line 14*/
-    [image7, image8],     /*Line 15*/
-    [image9, image10],    /*Line 16*/
+    [fake14],   /*Line 14 - image 11-14*/
+    [fake15],     /*Line 15 - image 7-8*/
+    [fake16],    /*Line 16 - image 9-10*/
     "",    /*Line 17*/
     "Ignore wicks, Big days come after small bodies",               /*Line 18*/
     "",     /*Line 19*/
@@ -47,8 +56,8 @@ export const toolTips = [
     [weekly1, weekly2, weekly3, weekly4, weekly5, weekly6],          /*Line 28*/
     [image1, image2, image3, image4, image5, image6],                /*Line 29*/
     "",    /*Line 30*/
-    "",      /*Line 31*/
-    "",    /*Line 32*/
+    [avgWeekly],      /*Line 31*/
+    [avgDaily],    /*Line 32*/
     "", /*Line 33*/
     "",    /*Line 34*/
     "",    /*Line 35*/
